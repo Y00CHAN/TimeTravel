173d677e4b5bfc1d74a1cc83e0efcb13ac97f2fe
edwardLee <ghfy1245.dev@gmail.com>
2025-07-09 14:51:00 +0900

    spots routes 만드는 중

--- backend/timetraveler/urls.py
+++ backend/timetraveler/urls.py
@@ -20,5 +20,5 @@ from django.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/accounts/', include('accounts.urls')),
+    path('api/v1/users/', include('accounts.urls')),
 ]
